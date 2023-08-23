5caaece8de1bc999fda5d60cb2b7bdf38cf8bd88
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2023-08-23 05:53:15 -0700

    style: Change fonts on texts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,44 +3,62 @@ import Facebook from "../../assets/icons/Facebook.png";
 import Youtube from "../../assets/icons/Youtube.png";
 import Instagram from "../../assets/icons/Instagram.png";
 
-
 function Footer() {
   return (
-    <div className='bg-black'>
-        <div className='flex justify-center items-center pt-[44px]'>
-
+    <div className="bg-black">
+      <div className="flex justify-center items-center pt-[44px]">
         <img src={FooterLogo} />
-        </div>
+      </div>
 
-        <div className='flex justify-center items-center gap-20 text-white pt-[52px] pb-[30px]'>
-            <p>Our Services</p>
-            <p>Help</p>
-            <p>About Us</p>
-            <p>Contact US</p>
-        </div>
+      <div className="flex justify-center items-center gap-20 text-white pt-[52px] pb-[30px]">
+        <p className="font-normal text-[35px] leading-[47.67px] tracking-[6%]">
+          Our Services
+        </p>
+        <p className="font-normal text-[35px] leading-[47.67px] tracking-[6%]">
+          Help
+        </p>
+        <p className="font-normal text-[35px] leading-[47.67px] tracking-[6%]">
+          About Us
+        </p>
+        <p className="font-normal text-[35px] leading-[47.67px] tracking-[6%]">
+          Contact US
+        </p>
+      </div>
 
-        <div className='w-full h-px bg-white'></div>
+      <div className="w-full h-px bg-white"></div>
 
-        <div className='flex justify-center items-center gap-20 text-white pt-[52px] pb-[30px]'>
-            <p>Plans & Pricing</p>
-            <p>How it works</p>
-            <p>What is ShipCo</p>
-            <p>Support@shipper.com</p>
-        </div>
+      <div className="flex justify-center items-center gap-20 text-white pt-[52px] pb-[30px]">
+        <p className="font-normal text-[35px] leading-[47.67px] tracking-[1.5%]">
+          Plans & Pricing
+        </p>
+        <p className="font-normal text-[35px] leading-[47.67px] tracking-[1.5%]">
+          How it works
+        </p>
+        <p className="font-normal text-[35px] leading-[47.67px] tracking-[1.5%]">
+          What is ShipCo
+        </p>
+        <p className="font-normal text-[35px] leading-[47.67px] tracking-[1.5%]">
+          Support@shipper.com
+        </p>
+      </div>
 
-        <div className='flex justify-center items-center gap-[21px] pt-[46px] pb-[30px]'>
-    <img src={Facebook} />
-    <img src={Youtube} />
-    <img src={Instagram}  />
-        </div>
+      <div className="flex justify-center items-center gap-[21px] pt-[46px] pb-[30px]">
+        <img src={Facebook} />
+        <img src={Youtube} />
+        <img src={Instagram} />
+      </div>
 
-        <div className='flex justify-center items-center gap-[12px] text-white pb-[26px]'>
-            <p>Privacy Policy</p>
-            <p>|</p>
-            <p>Terms & Conditions</p>
-        </div>
+      <div className="flex justify-center items-center gap-[12px] text-white pb-[26px]">
+        <p className="font-normal text-base leading-[23.86px]">
+          Privacy Policy
+        </p>
+        <p className="font-normal text-base leading-[23.86px]">|</p>
+        <p className="font-normal text-base leading-[23.86px]">
+          Terms & Conditions
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
