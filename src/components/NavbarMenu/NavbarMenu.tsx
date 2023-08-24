66d8adbd20d35c7d2d9bbce44a80ad1926762603
anrikopaliani <anrikopa@gmail.com>
2023-08-24 11:18:48 +0100

    style:responsive navbar

---
+++ src/components/NavbarMenu/NavbarMenu.tsx
@@ -0,0 +1,52 @@
+import  { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import NavbarItem from "./NavbarItem";
+
+import {MdOutlineClose} from "react-icons/md"
+import {GiHamburgerMenu} from "react-icons/gi"
+
+const NavbarMenu = () => {
+  const [isToggled, setToggle] = useState(false);
+
+  const navContainer = {
+    visible: {
+      //x: 0,
+      opacity: 1,
+      transition: {
+        x: { velocity: 100 },
+        duration: 0.3
+      }
+    },
+    hidden: {
+      //x: -250,
+      opacity: 0,
+      transition: {
+        x: { velocity: 100 },
+        duration: 0.3
+      }
+    }
+  };
+
+  return (
+    <div className="lg:hidden">
+      <button className="btn absolute right-2" onClick={() => setToggle(!isToggled)}>
+            {isToggled ? <MdOutlineClose size={20}/> : <GiHamburgerMenu size={20}/>}
+      </button>
+      <AnimatePresence>
+        {isToggled && (
+          <motion.div
+            className="navbar absolute top-0 right-0 min-h-screen"
+            initial="hidden"
+            animate={isToggled ? "visible" : "hidden"}
+            exit="hidden"
+            variants={navContainer}
+          >
+            <NavbarItem isToggled={isToggled} />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default NavbarMenu;
