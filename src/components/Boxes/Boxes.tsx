0cdeea50b30ccdb26ae1a0a77102cd8a2adf4634
anrikopaliani <anrikopa@gmail.com>
2023-08-24 11:25:21 +0100

    style:button gap

--- src/components/Boxes/Boxes.tsx
+++ src/components/Boxes/Boxes.tsx
@@ -3,65 +3,55 @@ import Payment from "../../assets/icons/Payment.png";
 import Vector from "../../assets/icons/Vector.png";
 import Repack from "../../assets/icons/Repack.png";
 import Delivery from "../../assets/icons/Delivery.png";
-import Rectangle from "../../assets/icons/Rectangle_options.png"
+import Rectangle from "../../assets/icons/Rectangle_options.png";
 
 function Boxes() {
   return (
     <>
+      <div className="relative flex justify-center items-center boxes-desktop">
+        <img src={Rectangle} />
+        <div className="absolute flex justify-center items-center gap-32">
+          <div className="flex flex-col gap-6 justify-center items-center">
+            <div className="rounded-full bg-white p-8 flex justify-center items-center">
+              <img src={Payment} />
+            </div>
 
-        <div className="relative flex justify-center items-center boxes-desktop">
-      <img src={Rectangle} />
-      <div className="absolute flex justify-center items-center gap-32">
-
-        <div className="flex flex-col gap-6 justify-center items-center">
-          <div className="rounded-full bg-white p-8 flex justify-center items-center">
-            <img src={Payment} />
+            <div className="w-[174px] h-[49px] bg-white rounded-full text-center flex justify-center items-center">
+              <p>Payment</p>
+            </div>
           </div>
 
-          <div className="w-[174px] h-[49px] bg-white rounded-full text-center flex justify-center items-center">
-            <p>Payment</p>
-          </div>
-        </div>
+          <div className="flex flex-col gap-6 justify-center items-center">
+            <div className="rounded-full bg-white p-8 flex justify-center items-center">
+              <img src={Vector} />
+            </div>
 
-
-        <div className="flex flex-col gap-6 justify-center items-center">
-          <div className="rounded-full bg-white p-8 flex justify-center items-center">
-            <img src={Vector} />
-          </div>
-
-          <div className="w-[174px] h-[49px] bg-white rounded-full text-center flex justify-center items-center">
-            <p>Calculator</p>
-          </div>
-        </div>
-
-
-        <div className="flex flex-col gap-6 justify-center items-center">
-          <div className="rounded-full bg-white p-8 flex justify-center items-center">
-            <img src={Repack} />
+            <div className="w-[174px] h-[49px] bg-white rounded-full text-center flex justify-center items-center">
+              <p>Calculator</p>
+            </div>
           </div>
 
-          <div className="w-[174px] h-[49px] bg-white rounded-full text-center flex justify-center items-center">
-            <p>Repack</p>
-          </div>
-        </div>
+          <div className="flex flex-col gap-6 justify-center items-center">
+            <div className="rounded-full bg-white p-8 flex justify-center items-center">
+              <img src={Repack} />
+            </div>
 
-        <div className="flex flex-col gap-6 justify-center items-center">
-          <div className="rounded-full bg-white p-8 flex justify-center items-center">
-            <img src={Delivery} />
+            <div className="w-[174px] h-[49px] bg-white rounded-full text-center flex justify-center items-center">
+              <p>Repack</p>
+            </div>
           </div>
 
-          <div className="w-[174px] h-[49px] bg-white rounded-full text-center flex justify-center items-center">
-            <p>Delivery</p>
+          <div className="flex flex-col gap-6 justify-center items-center">
+            <div className="rounded-full bg-white p-8 flex justify-center items-center">
+              <img src={Delivery} />
+            </div>
+
+            <div className="w-[174px] h-[49px] bg-white rounded-full text-center flex justify-center items-center">
+              <p>Delivery</p>
+            </div>
           </div>
         </div>
-
-
-
-
-
       </div>
-    </div>
-
 
       <div className="flex flex-col items-center mx-auto md:flex-row boxes-mobile">
         <div className="w-full max-w-screen-3xl mx-auto">
@@ -138,7 +128,6 @@ function Boxes() {
           </div>
         </div>
       </div>
-
     </>
   );
 }
